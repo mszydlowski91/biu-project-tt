db307c27ef6465b20f11f0c91357205503304955
Joanna Sykurska <j.sykurska@gmail.com>
2015-04-28 22:02:43 +0200

    addPost controller

--- js/firstcontroller.js
+++ js/firstcontroller.js
@@ -1,1 +1,15 @@
-angular.module('first', []).controller();
+angular.module('first', []).
+controller('postController', ['$scope', function($scope) {
+    	$scope.postList=[];
+
+     $scope.addPost=function(){
+     	console.log("YOLO");
+     	 var post={title:$scope.title,
+			text:$scope.text
+		};
+
+		$scope.postList.push(post);
+		  }
+
+
+    }]);
