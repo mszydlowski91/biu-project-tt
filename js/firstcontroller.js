de82633edd0ca77398d6d675561dc085d0def09f
marcin.rogi <199595@edu.p.lodz.pl>
2015-04-29 01:46:37 +0200

    no message

--- js/firstcontroller.js
+++ js/firstcontroller.js
@@ -2,20 +2,20 @@ var app = angular.module("first", []);
 
 app.controller("postController", function($scope, $http) {
   $http.get('http://private-79b25-blogtt.apiary-mock.com/posts').
+  success(function(data, status, headers, config) {
+    $scope.postList = data;
+  }).
+  error(function(data, status, headers, config) {
+    console.log("error getting");
+  })
+  $scope.newPost = function(){
+    $http.post('http://private-79b25-blogtt.apiary-mock.com/posts', {title: $scope.title, text: $scope.text }).
     success(function(data, status, headers, config) {
-      $scope.postList = data;
+      
     }).
     error(function(data, status, headers, config) {
-      console.log("error getting");
-    });
-
-
-$http.post('http://private-79b25-blogtt.apiary-mock.com/posts', {title: $scope.title, text: $scope.text }).
-  success(function(data, status, headers, config) {
-
-  }).
- error(function(data, status, headers, config) {
      console.log("error posting");
-  });
+    });
+  }
 });
 
