c7a4bbf8ca98197522dd13b0f6a2536530c845f1
Joanna Sykurska <j.sykurska@gmail.com>
2015-04-29 01:24:08 +0200

    printing from API

--- js/firstcontroller.js
+++ js/firstcontroller.js
@@ -1,15 +1,21 @@
-angular.module('first', []).
-controller('postController', ['$scope', function($scope) {
-    	$scope.postList=[];
+var app = angular.module("first", []);
 
-     $scope.addPost=function(){
-     	console.log("YOLO");
-     	 var post={title:$scope.title,
-			text:$scope.text
-		};
-
-		$scope.postList.push(post);
-		  }
+app.controller("postController", function($scope, $http) {
+  $http.get('http://private-79b25-blogtt.apiary-mock.com/posts').
+    success(function(data, status, headers, config) {
+      $scope.postList = data;
+    }).
+    error(function(data, status, headers, config) {
+      console.log("error getting");
+    });
 
 
-    }]);
+$http.post('http://private-79b25-blogtt.apiary-mock.com/posts', {title: $scope.title, text: $scope.text }).
+  success(function(data, status, headers, config) {
+
+  }).
+ error(function(data, status, headers, config) {
+     console.log("error posting");
+  });
+});
+
