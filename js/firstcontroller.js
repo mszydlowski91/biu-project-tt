d50adda1c081ee63006afb2754b8019efdd5f4f4
Michał <michal.k.szydlowski@gmail.com>
2015-05-04 23:24:40 +0200

    Cancelling edition and asking for confirmation upon post deletion.

--- js/firstcontroller.js
+++ js/firstcontroller.js
@@ -1,6 +1,11 @@
 var app = angular.module("first", []);
 
 app.controller("postController", function($scope, $http) {
+
+    var temp;
+
+
+
     $http.get('http://private-79b25-blogtt.apiary-mock.com/posts')
         .success(function(data, status, headers, config) {
             $scope.postList = data;
@@ -30,15 +35,35 @@ app.controller("postController", function($scope, $http) {
   	};
 
     $scope.deletePost = function(post){
-        var i = $scope.postList.indexOf(post);
-        $scope.postList.splice(i,1);
+        var del = confirm("Are you sure you want to delete or modify this post?");
+        if(del){ 
+            var i = $scope.postList.indexOf(post);
+            $scope.postList.splice(i,1);
+        }
+       
     };
 
-    $scope.editPost = function (post) {
+    $scope.editPost = function (post) {        
+   
         $scope.deletePost(post);
         $scope.form_header = "Edit post";
         $scope.title = post.title;
         $scope.text = post.text;
+        temp = post;
     }
+
+    $scope.cancelEdit = function (){
+        $http.post('http://private-79b25-blogtt.apiary-mock.com/posts', temp).
+            success(function(data, status, headers, config) {
+                $scope.postList.push(temp);
+        }).
+        error(function(data, status, headers, config) {
+            console.log("error posting");
+        });
+
+        $scope.title = "";
+        $scope.text = "";
+    }
+
 });
 
