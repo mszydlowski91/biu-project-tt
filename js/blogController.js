5dc4394a3dd69ee045e5f359758740594003c6dd
marcin.rogi <199595@edu.p.lodz.pl>
2015-05-18 22:12:37 +0200

    Post editing done and connected with apiary. Added simple form validation function. Code formatted.

--- js/blogController.js
+++ js/blogController.js
@@ -22,44 +22,86 @@ app.controller("blogController", function($scope, $http) {
             title: $scope.title,
             text: $scope.text,
             published_at: new Date()
-          };
-
-	    $http.post(path+'/posts', post)
-        .success(function(data, status, headers, config) {
-	            $scope.postList.push(post);
-	             })
-        .error(function(data, status, headers, config) {
-	     	console.log("error posting " + status);
-	       });
-
-        $scope.title = "";
-        $scope.text = "";
+        };
+        if (validateForm(post.title,post.text)) {
+          $http.post(path+'/posts', post)
+            .success(function(data, status, headers, config) {
+                $scope.postList.push(post);
+            })
+            .error(function(data, status, headers, config) {
+     	          console.log("error posting " + status);
+            });
+        }
+        //$scope.text = $scope.initial;
+        $scope.title = $scope.initial;
+        $scope.text = $scope.initial;
   	};
+
+    var validateForm = function(title,text){
+        var titleCheck = Boolean(title);
+        var textCheck = Boolean(text);
+        var result = titleCheck && textCheck;
+        return result;
+    };
 
     $scope.deletePost = function(post){
         var del = confirm("Are you sure you want to delete this post?");
         if(del){
-            var i = $scope.postList.indexOf(post);
-            var delPost={
-                "delete_id": 1
-            };
-
-            $http.delete(path+'/posts', delPost)
-              .success(function(data, status, headers, config) {
+          var i = $scope.postList.indexOf(post);
+          var delPost={
+              "delete_id": i
+          };
+          $http.delete(path+'/posts', delPost)
+            .success(function(data, status, headers, config) {
                 $scope.postList.splice(i,1);
-      	             })
-              .error(function(data, status, headers, config) {
-      	     	console.log("Error during post deleting: " + status);
-      	       });
+            })
+            .error(function(data, status, headers, config) {
+                console.log("Error during post deleting: " + status);
+            });
         }
     };
 
+    var editedPostindex;
+    var existingPosts;
     $scope.editPost = function (post) {
         $scope.formMode = "EDIT";
+        editedPostindex = $scope.postList.indexOf(post);
+        existingPosts = $scope.postList;
+        $scope.postList = [post];
+        $scope.text = post.text;
+        $scope.title = post.title;
+    };
+
+    $scope.confirmEdit = function(){
+        if (validateForm($scope.title, $scope.text)) {
+          var editedPost = {
+            "post_id": editedPostindex,
+            "new_title": $scope.title,
+            "new_text": $scope.text
+          };
+          $http.put(path+'/posts', editedPost)
+            .success(function(data, status, headers, config) {
+                existingPosts[editedPostindex].title=$scope.title;
+                existingPosts[editedPostindex].text=$scope.text;
+                $scope.postList = existingPosts;
+                $scope.formMode = "NEW";
+                $scope.title = $scope.initial;
+                $scope.text = $scope.initial;
+                existingPosts = null;
+                editedPostindex = null;
+            })
+            .error(function(data, status, headers, config) {
+                console.log("Error during post editing: " + status);
+            });
+        }
     };
 
     $scope.cancelEdit = function (){
         $scope.formMode = "NEW";
+        $scope.postList = existingPosts;
+        //$scope.text = $scope.initial;
+        $scope.title = "";
+        $scope.text = "";
     };
 
 
